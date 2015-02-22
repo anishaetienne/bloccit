8303138f1f4e4a7b22147af5b4684c915ddb6264
Anisha Etienne <anisha.etienne@icloud.com>
2015-02-22 02:33:48 -0700

    Intro to RSpec

--- Gemfile
+++ Gemfile
@@ -66,5 +66,9 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 
+group :test do
+  gem 'rspec-rails', '~> 3.1.0'
+end
+
 end
 
---
+++ spec/vote_spec.rb
@@ -0,0 +1,16 @@
+describe Vote do 
+  describe "validations" do
+    describe "value validation" do
+      it "only allows -1 or 1 as values" do
+        valid = Vote.new(value: 1)
+        expect(v.valid?).to eq(true)
+
+        valid2 = Vote.new(value: -1)
+        expect(v.valid).to eq(true)
+
+        invalid_3 = Vote.new(value: 2)
+        expect(invalid_3.valid).to eq(false)
+      end
+    end
+  end
+end
