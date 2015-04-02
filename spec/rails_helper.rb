d1fb7d9752f7705f6e780607fc42e98f16443aef
Anisha Etienne <anisha.etienne@icloud.com>
2015-04-02 00:57:47 -0600

    Testing Users

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,10 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  
+  # Make Factory Girl's methods available
+   config.include FactoryGirl::Syntax::Methods
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
@@ -47,4 +51,5 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
 end
